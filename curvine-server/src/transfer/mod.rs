7c9ed431e19790cd27cdfbe09dfba09b8b14c8d0
John <hellojlong@163.com>
2026-07-27 20:33:30 +0800

    feat(transfer): add MySQL metadata store

--- curvine-server/src/transfer/mod.rs
+++ curvine-server/src/transfer/mod.rs
@@ -6,3 +6,6 @@ pub use self::memory_store::MemoryTransferStore;
 
 mod sqlite_store;
 pub use self::sqlite_store::SqliteTransferStore;
+
+mod mysql_store;
+pub use self::mysql_store::MysqlTransferStore;

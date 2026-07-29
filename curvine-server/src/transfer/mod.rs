0d910d516f8da963ceeeccd6ec3d822465b386f8
John <hellojlong@163.com>
2026-07-29 16:08:19 +0800

    feat(transfer): add store backend and cluster cache

--- curvine-server/src/transfer/mod.rs
+++ curvine-server/src/transfer/mod.rs
@@ -9,3 +9,12 @@ pub use self::sqlite_store::SqliteTransferStore;
 
 mod mysql_store;
 pub use self::mysql_store::MysqlTransferStore;
+
+mod metrics;
+pub use self::metrics::{MetadataReplicaRefreshObservation, TransferMetrics};
+
+mod backend;
+pub use self::backend::TransferStoreBackend;
+
+mod cluster_cache;
+pub use self::cluster_cache::ClusterMetadataCache;
